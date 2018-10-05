853559d5fe9c2dd3ca2c2e5d39b74eb5cfa54e5f
eryson <eryson@vendabem.com.br>
2018-10-05 16:34:14 -0300

    Add comment options

--- blog/urls.py
+++ blog/urls.py
@@ -15,4 +15,10 @@ urlpatterns = [
     #url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     path('post/<int:pk>/remove', views.post_remove, name='post_remove'),
     #url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment', views.add_comment_to_post, name='add_comment_to_post'),
+    #url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve', views.comment_approve, name='comment_approve'),
+    #url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove', views.comment_remove, name='comment_remove'),
+    #url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
